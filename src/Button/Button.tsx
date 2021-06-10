bce9db46e1a20fa8a1e6fb5e0e5277850c0ed906
Igor Razvodov <razvodovigor.dev@gmail.com>
2021-06-10 13:08:40 +0300

    Set primary true by default

--- src/Button/Button.tsx
+++ src/Button/Button.tsx
@@ -28,7 +28,7 @@ export interface ButtonProps {
  * Primary UI component for user interaction
  */
 export const Button: React.FC<ButtonProps> = ({
-  primary = false,
+  primary = true,
   size = 'medium',
   backgroundColor,
   label,
